6a7d05554e24cbce0f742ea1928eb7c49312c490
Mihaela C <mchiaburu.1337@gmail.com>
2025-04-07 10:08:34 +0300

    small changes

--- Lab4/OnlineStore/OnlineStore/obj/Debug/net8.0/OnlineStore.AssemblyInfo.cs
+++ Lab4/OnlineStore/OnlineStore/obj/Debug/net8.0/OnlineStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54a7e669f417ccc1626a870355e46f9f426dc73c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7485ebe705fc6110c717836450c891547438f7fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
