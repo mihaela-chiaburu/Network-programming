8949d6e905f806ebc5266b2379c5eb260d01a099
Mihaela Chiaburu <mchiaburu.1337@gmail.com>
2025-08-22 22:34:34 +0300

    git ignore 2

--- Lab6 - NTP/NTPClientApp/obj/Debug/net8.0-windows/NTPClientApp.AssemblyInfo.cs
+++ Lab6 - NTP/NTPClientApp/obj/Debug/net8.0-windows/NTPClientApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NTPClientApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77386f5567457b1fabda55e7d22d5c3362834de0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+052f124f5271bd72d538cf54d9d7e02ec5276b35")]
 [assembly: System.Reflection.AssemblyProductAttribute("NTPClientApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NTPClientApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
